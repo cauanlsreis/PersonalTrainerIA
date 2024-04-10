c788d517524a6d90ef06c5215857c08264b7041f
cauanlsreis <cauanlucas.reis@gmail.com>
2024-04-10 14:51:24 -0300

    Montando a lógica de flexões

--- personalai_dash.py
+++ personalai_dash.py
@@ -10,10 +10,27 @@ personalAI = PersonalAI("IMG_2149.mp4")
 personalAI.run(True)
 
 placeholder = st.empty()
+
+count = 0
+
 while True:
     frame,landmarks, ts = personalAI.image_q.get()
     
     if len(landmarks.pose_landmarks)>0:
         frame, elbow_angle = personalAI.find_angle(frame, landmarks, 12, 14, 16, True)
+        frame, hip_angle = personalAI.find_angle(frame, landmarks, 11, 23, 25, True)
+
+        #Lógica para as flexões
+        if elbow_angle > 150 and hip_angle > 170:
+            status = "ready"
+            dir = "down"
+        if status == "ready":
+            if dir == "down" and elbow_angle < 60:
+                dir = "up"
+                count += 0.5
+            if dir == "up" and elbow_angle > 100:
+                dir = "down"
+                count += 0.5
+                
         with placeholder.container():
             st.image(frame)
